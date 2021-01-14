e707eb200674a2b7bbdbdec102babbc85adca7a3
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-14 12:03:06 -0500

    Completed examples and notes for closures with multiple parameters.

---
+++ Closures.playground/Pages/Closures Part 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//CLOSURES WITH MULTIPLE PARAMETERS
+//Closure requires two parameters: where the person is going and the speed they are travelling at
+
+func travel(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+//Uses shorthand parameter names for the two parameters ($0 and $1)
+
+travel {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+
+
+
+
+//: [Next](@next)
