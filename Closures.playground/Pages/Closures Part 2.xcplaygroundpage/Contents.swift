8f48bdf8b5774283b4eea1bb2f7a5fe57fd9f589
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-14 20:32:35 -0500

    Using closures as parameters when they return values.

--- Closures.playground/Pages/Closures Part 2.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/Closures Part 2.xcplaygroundpage/Contents.swift
@@ -45,6 +45,22 @@ travel { (place: String) in
     print("I'm going to \(place) in my car")
 }
 
+//USING CLOSURES AS PARAMETERS WHEN THEY RETURN VALUES
+//Void used to mean “accepts no parameters and returns nothing”, but can replace that Void with any type of data to force the closure to return a value
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+
+
+
 
 
 
