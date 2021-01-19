78cf3330acd359c02c19ac249a047a3c7b01c245
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-19 16:46:35 -0500

    Structures vs classes code example

--- Structure and Classes.playground/Contents.swift
+++ Structure and Classes.playground/Contents.swift
@@ -1,33 +1,39 @@
 import Cocoa
 
-struct Location {
+class Location {
     var x: Int
     var y: Int
-}
-
-//September at LCS
-var home = Location(x: 7, y: 10)
-var school = home
-
-//Where is home
-home.x
-home.y
-
-//Where is school
-school.x
-school.y
-
-//Christmas vacation time
-home.x = 19
-home.y = 10
-
-//Where is home
-home.x
-home.y
-
-//Where is school
-school.x
-school.y
-
-
-
+    
+    
+    
+    init(x: Int, y: Int){
+        self.x = x
+        self.y = y
+    }
+    
+    //September at LCS
+    var home = Location(x: 7, y: 10)
+    var school = home
+    
+    //Where is home
+    home.x
+    home.y
+    
+    //Where is school
+    school.x
+    school.y
+    
+    //Christmas vacation time
+    home.x = 19
+    home.y = 10
+    
+    //Where is home
+    home.x
+    home.y
+    
+    //Where is school
+    school.x
+    school.y
+    
+    
+    
